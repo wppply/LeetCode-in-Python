c7c5f6a0d6c3c544d95f36c7052bc405429ff8a8
wppply <wppply@gmail.com>
2018-06-27 06:45:31 +0800

    6/27/18

---
+++ p16.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums = sorted(nums)
+        min_diff = float('inf')
+        best = 0
+        for k in range(len(nums)):
+        	tempTarget = target - nums[k]
+
+        	i, j = k+1, len(nums)-1
+        	while i < j:
+        		s = nums[k] + nums[i] + nums[j]
+        		diff = s - target
+        		if abs(diff) < min_diff:
+        			min_diff = abs(diff)
+        			best = s
+
+        		if diff > 0:
+        			j -= 1
+        		else:
+        			i += 1
+        return best
+
+if __name__ == "__main__":
+	sol = Solution()
+	nums = [1,3,4,51,123,12,34,53,21,2,57]
+	target = 100
+	print(sol.threeSumClosest(nums,target))
+
+
+
+
---
+++ p24.py
@@ -0,0 +1,17 @@
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+        	return head
+        dummyNode = ListNode(0)
+        dummyNode.next = head.next
+
+        last
