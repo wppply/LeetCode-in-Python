700a686755af48993635d848ad597ce0a5b8f6b8
wppply <wppply@gmail.com>
2018-08-19 23:44:45 +0800

    8/20/18

---
+++ p347.py
@@ -0,0 +1,18 @@
+from collections import Counter
+import heapq
+
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = Counter(nums)
+        heap = [(-ct, v) for v, ct in count.items()]
+        heapq.heapify(heap)
+        return [heapq.heappop(heap)[1] for _ in range(k)]
+
+
+
